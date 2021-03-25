8272894b5569aad4c85e9b8e1ab5ae6c5eb8d164
Yohei Zaiznen <20seikisyounen@gmail.com>
2021-03-26 01:50:19 +0900

    wip

---
+++ get_gitlab_discussion.py
@@ -0,0 +1,30 @@
+import requests
+import MeCab
+from wordcloud import WordCloud
+
+url = "https://api.github.com/repos/hashicorp/terraform/issues/comments?per_page=100&page=2"
+
+r = requests.get(url)
+
+body_str = ''
+for node in r.json():
+  body_str += node['body']
+
+print(body_str)
+
+tagger = MeCab.Tagger()
+tagger.parse('')
+node = tagger.parseToNode(r.text)
+
+word_list = []
+while node:
+    word_type = node.feature.split(',')[0]
+    if word_type == '名詞':
+        word_list.append(node.surface)
+    node = node.next
+
+word_chain = ' '.join(word_list)
+
+W = WordCloud(width=640, height=480, background_color='white', colormap='bone', font_path='C:\Windows\Fonts\yumin.ttf').generate(word_chain)
+
+W.to_file('W.png')
