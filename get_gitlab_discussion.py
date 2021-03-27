624795e6c918e1413af952cc6463c02fda09ce99
Yohei Zaiznen <20seikisyounen@gmail.com>
2021-03-28 02:18:34 +0900

    create page

--- get_gitlab_discussion.py
+++ get_gitlab_discussion.py
@@ -5,7 +5,7 @@ from wordcloud import WordCloud
 url = "https://api.github.com/repos/hashicorp/terraform/issues/comments?per_page=100&page="
 body_str = ''
 
-for i in range(1):
+for i in range(10):
     r = requests.get(url+str(i))
     for node in r.json():
         body_str += node['body']
@@ -26,4 +26,4 @@ word_chain = ' '.join(word_list)
 
 W = WordCloud(width=640, height=480, background_color='white', colormap='bone', font_path='/usr/share/fonts/truetype/fonts-japanese-gothic.ttf').generate(word_chain)
 
-W.to_file('W.png')
+W.to_file('wordcloud.png')
