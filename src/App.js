feac9b1ef41b1bca2d15f4db8b34169acd64c0c9
Yohei Zaiznen <20seikisyounen@gmail.com>
2021-03-28 02:51:06 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={wordcloud} className="App-logo" alt="logo" />
+        <img src={wordcloud}/>
         <p>
           Terraform Repository Issue comment wordcloud
         </p>
